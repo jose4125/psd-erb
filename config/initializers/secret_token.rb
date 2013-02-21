0e402d5cc4369ce100496a6b3ff8e7f7b782097b
jose david lombana <jose4125@gmail.com>
2013-02-21 17:09:03 -0500

    psd to erb

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ejer2::Application.config.secret_token = '99b61322df174bdb8e2c3c96b9e313779137db49de4eaa44c2bbc0185488f3b7fd68751a92e87340cd4b7b4a97985ff37114b75139f26de2a12847a4eb1fca12'
